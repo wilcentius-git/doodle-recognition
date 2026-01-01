f147460e9500f4ee7d339efb1dba52ba6dc739ba
0xelsalemi <futurebiginvestor@gmail.com>
2026-01-01 16:08:19 +0700

    Update models_dl.py

--- src/models_dl.py
+++ src/models_dl.py
@@ -2,23 +2,6 @@ import torch.nn as nn
 from torchvision import models
 from src.config import IMG_SIZE
 
-class TunedMLP(nn.Module):
-    def __init__(self, num_classes):
-        super(TunedMLP, self).__init__()
-        self.flatten = nn.Flatten()
-        self.layers = nn.Sequential(
-            nn.Linear(IMG_SIZE * IMG_SIZE, 1024),
-            nn.ReLU(),
-            nn.Dropout(0.5), 
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(512, num_classes)
-        )
-    def forward(self, x):
-        x = self.flatten(x)
-        return self.layers(x)
-
 class TunedCNN(nn.Module):
     def __init__(self, num_classes):
         super(TunedCNN, self).__init__()
@@ -43,11 +26,4 @@ def get_resnet18(num_classes):
     model = models.resnet18(weights=None)
     model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
     model.fc = nn.Linear(model.fc.in_features, num_classes)
-    return model
-
-def get_mobilenet_v2(num_classes):
-    model = models.mobilenet_v2(weights=None)
-    model.features[0][0] = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias=False)
-    model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
-
     return model
