fd515608ba719a843fda75444eb44977441843bc
0xelsalemi <futurebiginvestor@gmail.com>
2026-01-01 16:08:46 +0700

    Remove SVC model definition from models_ml.py

--- src/models_ml.py
+++ src/models_ml.py
@@ -1,5 +1,4 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
 
 def get_rf_model():
     return RandomForestClassifier(
@@ -11,12 +10,3 @@ def get_rf_model():
         random_state=42
     )
 
-def get_svm_model():
-    return SVC(
-        probability=True, 
-        kernel='linear',  # Linear bagus untuk high-dimensional
-        C=10,
-        class_weight='balanced',
-        random_state=42,
-        max_iter=2000
-    )
